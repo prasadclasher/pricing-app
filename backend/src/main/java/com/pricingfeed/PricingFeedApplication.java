d0842b05e680f8a6c95aff9a2e8e034ae7971f5b
Prasad <prasadclasher@gmail.com>
2026-05-21 09:20:16 +0530

    Create PricingFeedApplication.java

---
+++ backend/src/main/java/com/pricingfeed/PricingFeedApplication.java
@@ -0,0 +1,11 @@
+package com.pricingfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricingFeedApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PricingFeedApplication.class, args);
+    }
+}
